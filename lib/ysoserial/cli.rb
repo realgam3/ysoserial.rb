f5e43c6c15281df722a6976542c04dbd7b2e1646
realgam3 <realgam3@gmail.com>
2021-02-10 21:29:40 +0200

    remove argumnets that is not supported yet

--- lib/ysoserial/cli.rb
+++ lib/ysoserial/cli.rb
@@ -7,25 +7,11 @@ ysoserial.rb generates deserialization payloads for a variety of ruby formatters
 Usage: ysoserial.rb [options]
 
 Options:
-  -o, --output=VALUE         The output format (raw|escaped|base64). [Default: raw]
+  -o, --output=VALUE         The output format (raw|escaped|base64). [default: raw]
   -g, --gadget=VALUE         The gadget chain.
-  -f, --formatter=VALUE      The formatter (YAML|Marshal). [Default: Marshal]
-  -c, --command=VALUE        The command to be executed. [Default: id 1>&2]
-  -s, --stdin                The command to be executed will be read from 
-                               standard input.
-  -t, --test                 Whether to run payload locally. [Default: false]
-      --raf, --runallformatters
-                             Whether to run all the gadgets with the provided 
-                               formatter (ignores gagdet name, output format, 
-                               and the test flag). This will search in 
-                               formatters and also show the displayed payload 
-                               length. [Default: false]
-      --sf, --searchformatter=VALUE
-                             Search in all formatters to show relevant 
-                               gadgets and their formatters (other parameters 
-                               will be ignored).
-      --debugmode            Enable debugging to show exception errors and 
-                               output length
+  -f, --formatter=VALUE      The formatter (yaml|marshal). [default: marshal]
+  -c, --command=VALUE        The command to be executed. [default: id 1>&2]
+  -t, --test                 Whether to run payload locally. [default: false]
   -h, --help                 Shows this message and exit.
 
 DOCOPT
@@ -52,9 +38,6 @@ module YSoSerial
 
       gadget_class = gadgets[gadget_symbol]
       command = options["--command"]
-      # if options["--stdin"]
-      #   command = gets.chomp
-      # end
       gadget = gadget_class.new(command)
 
       function_args = [
